cb6306868c1a96b43d687127c3b81322bb1b2849
llallonen <lerushok@ural3000.local>
2022-08-04 21:36:33 +0500

    create an application skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Nav from './components/UI/navigate/Nav';
+import About from './pages/About';
+import Posts from './pages/Posts';
+import NotFound from './pages/NotFound';
+import AppRouter from './components/UI/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Nav />
+      <AppRouter />
+    </BrowserRouter>
+  )
 }
 
 export default App;
