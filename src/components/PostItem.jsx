e408b9be18caf05e4c0fbff3922667c339f8c581
llallonen <lerushok@ural3000.local>
2022-08-04 22:36:28 +0500

    add wrapper for post buttons and styles for it

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -4,16 +4,22 @@ import MyButton from './UI/buttons/MyButton';
 const PostItem = (props) => {
   return (
     <div className="post">
-    <div className="post__content">
-      <strong>{props.post.id}. {props.post.title}</strong>
-      <div>{props.post.body}</div>
+      <div className="post__content">
+        <strong>{props.post.id}. {props.post.title}</strong>
+        <div>{props.post.body}</div>
+      </div>
+      <div>
+        <div className='post__btns'>
+          <MyButton onClick={() => props.remove(props.post)} className="btn">
+            Open
+          </MyButton>
+          <MyButton onClick={() => props.remove(props.post)} className="btn">
+            Delete
+          </MyButton>
+        </div>
+
+      </div>
     </div>
-    <div>
-    <MyButton onClick={() => props.remove(props.post)} className="btn">
-      Delete
-    </MyButton>
-   </div>
-  </div>
   )
 }
 
