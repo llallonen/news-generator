fe682ea8e6f31e21e95dd4ed2dd3a7abac985409
llallonen <lerushok@ural3000.local>
2022-08-04 21:53:12 +0500

    add loader component

---
+++ src/components/UI/loader/MyLoader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './MyLoader.module.css';
+
+const loader = () => {
+  return (
+    <div className={classes.loader}></div>
+  )
+}
+
+export default loader
