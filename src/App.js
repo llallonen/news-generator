92288c6ed548d34aae40b4c10a353231acde276a
llallonen <lerushok@ural3000.local>
2022-08-04 22:10:01 +0500

    add main app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import './styles/App.css';
-import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
-import Nav from './components/UI/navigate/Nav';
-import About from './pages/About';
-import Posts from './pages/Posts';
-import NotFound from './pages/NotFound';
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/UI/AppRouter';
+import Nav from './components/UI/navigate/Nav';
+import './styles/App.css';
 
 function App() {
   return (
