54b3c85bfddf5f57037163db94382869e48775f7
llallonen <lerushok@ural3000.local>
2022-08-23 21:14:58 +0500

    change import and replace app router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './components/UI/AppRouter';
+import AppRouter from './components/AppRouter';
 import Nav from './components/UI/navigate/Nav';
+import AuthContext from './context';
 import './styles/App.css';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Nav />
-      <AppRouter />
-    </BrowserRouter>
+    <AuthContext.Provider value={{
+      isAuth,
+      setIsAuth
+    }}>
+      <BrowserRouter>
+        <Nav />
+        <AppRouter />
+      </BrowserRouter>
+    </AuthContext.Provider>
+
   )
 }
 
